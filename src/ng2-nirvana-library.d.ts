593cc97c3cc037a5719ced26c3cf0baecd736e33
Jason Cavaliere <jasoncavaliere@hotmail.com>
2017-02-07 07:14:50 -0600

    first attempt at auto publish

--- src/ng2-nirvana-library.d.ts
+++ src/ng2-nirvana-library.d.ts
@@ -1,1 +1,6 @@
+import { ModuleWithProviders } from '@angular/core';
+
 export {Serializer} from "./src/serializer";
+export declare class Ng2NirvanaModule {
+    static forRoot(): ModuleWithProviders;
+}
